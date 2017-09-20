e0f4266e315721abfcbf2f9201a86e755b296289
William Fortin <willyfortin@gmail.com>
2017-09-20 19:07:45 -0400

    Remove linebreak-style error eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,6 +25,7 @@ module.exports = {
   // add your custom rules here
   'rules': {
     "comma-dangle": 0,
+    "linebreak-style": 0,
     // don't require .vue extension when importing
     'import/extensions': ['error', 'always', {
       'js': 'never',
